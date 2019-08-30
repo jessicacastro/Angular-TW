dda283a5b187402730aa75ce3ff9e1d00918f59f
Jessica Castro <jessica.castros@outlook.com>
2019-08-30 15:48:22 -0300

    Novo componente no meu app e nova branch pokedex

--- meu-app/src/app/app.module.ts
+++ meu-app/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuComponenteComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    MeuComponenteComponent
+  ]
 })
 export class AppModule { }
